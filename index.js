ba7d74d1d873bbb48b67314dcfa0ad9e17b98812
tthomas89 <taylor.thomas000@gmail.com>
2018-06-14 16:39:37 -0700

    created a messages file to handle success instructions as well as printing RCA to console

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const figlet            = require('figlet');
-const chalk             = require('chalk');
-const clear             = require('clear');
+const chalk = require('chalk');
 
-const { url }           = require('./config/repo-config');
-const github            = require('./lib/github');
-const files             = require('./lib/files');
-const inquierer         = require('./lib/inquirer');
+const { url } = require('./config/repo-config');
 
-clear();
+const github = require('./lib/github');
+const files = require('./lib/files');
+const inquierer = require('./lib/inquirer');
+const messages = require('./lib/messages');
 
-console.log('');
-console.log (
-    chalk.yellow (
-        figlet.textSync('~~ R.C.A. ~~', { horizontalLayout: 'full' }))
-);
+messages.printRCA();
 
 const run = async () => {
-    const cloneDone = await github.cloneRepo(url);
-    const removeGitDone = await github.removeGit();
-    
-    if (cloneDone && removeGitDone) { 
-        const folderName = await inquierer.askFolderName();
-        if (folderName) {
-            files.renameFolder(folderName);
-            console.log(chalk.green('\n Success! \n')); 
-        }
+    const cloneDone = await github.cloneRepo(url); // clones react-starter repo
+    const removeGitDone = await github.removeGit(); // removes .git folder from within react-starter repo
 
+    if (cloneDone && removeGitDone) {
+        const folderName = await inquierer.askFolderName(); // asks user for input to rename react-starter folder
+        if (folderName) {
+            await files.renameFolder(folderName); // renames react-starter folder to what the user inputs. input must be longer than 3 characters
+            messages.printRCA();
+            console.log(chalk.green.bold('\n Success! \n'));
+            messages.printSuccessInstructions(folderName.foldername); // prints instructions to user (cd into folder, install packages, npm run start)
+        }
     } else {
         console.log(chalk.red('\nOops! Something went wrong..'));
         console.log(cloneDone + '\n');
