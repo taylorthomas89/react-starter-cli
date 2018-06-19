788fe9916a545e324d3cfed87d4db81b9ac3de3d
tthomas89 <taylor.thomas000@gmail.com>
2018-06-19 11:59:01 -0700

    refactored git clone of repo to be wrapped in a promise because the package itself doesn't support promises and was throwing a warning

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
+const chalk             = require('chalk');
+const CLI               = require('clui');
+const Spinner           = CLI.Spinner;
 
-const { url } = require('./config/repo-config');
-
-const github = require('./lib/github');
-const files = require('./lib/files');
-const inquierer = require('./lib/inquirer');
-const messages = require('./lib/messages');
+const { url }           = require('./config/repo-config');
+const github            = require('./lib/github');
+const files             = require('./lib/files');
+const inquierer         = require('./lib/inquirer');
+const messages          = require('./lib/messages');
 
 messages.printRCA();
 
 const run = async () => {
+    const status = new Spinner(chalk.green.bold('Fetching files... :) :) :)'));
+    status.start();
+
     const cloneDone = await github.cloneRepo(url); // clones react-starter repo
     const removeGitDone = await github.removeGit(); // removes .git folder from within react-starter repo
-
-    if (cloneDone && removeGitDone) {
+    
+    if (removeGitDone) {
+        status.stop();
         const folderName = await inquierer.askFolderName(); // asks user for input to rename react-starter folder
         if (folderName) {
             await files.renameFolder(folderName); // renames react-starter folder to what the user inputs. input must be longer than 3 characters
-            messages.printRCA();
-            console.log(chalk.green.bold('\n Success! \n'));
             messages.printSuccessInstructions(folderName.foldername); // prints instructions to user (cd into folder, install packages, npm run start)
         }
     } else {
+        status.stop();
         console.log(chalk.red('\nOops! Something went wrong..'));
         console.log(cloneDone + '\n');
     }
